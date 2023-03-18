da9f37017741692ca87e8a875835c5926de70ec1
SergeyAnshin <anshin.serega@gmail.com>
2023-03-18 23:34:57 +0300

    add @EnableScheduling annotation

--- src/main/java/com/example/weatheranalyzer/WeatherAnalyzerApplication.java
+++ src/main/java/com/example/weatheranalyzer/WeatherAnalyzerApplication.java
@@ -2,12 +2,13 @@ package com.example.weatheranalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherAnalyzerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherAnalyzerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAnalyzerApplication.class, args);
+    }
 }
