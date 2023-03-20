cdd8d8955c7e4e4714b1275efd08057273ce110f
SergeyAnshin <anshin.serega@gmail.com>
2023-03-20 14:22:02 +0300

    add findTopByLocationCityOrderByCreateDateDesc() method to WeatherRepository

--- src/main/java/com/example/weatheranalyzer/repository/WeatherRepository.java
+++ src/main/java/com/example/weatheranalyzer/repository/WeatherRepository.java
@@ -3,5 +3,9 @@ package com.example.weatheranalyzer.repository;
 import com.example.weatheranalyzer.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+
+    Optional<Weather> findTopByLocationCityOrderByCreateDateDesc(String city);
 }
