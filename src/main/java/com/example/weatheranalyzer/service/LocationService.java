ce75856134fe214de041d8777452b32f038d6aea
SergeyAnshin <anshin.serega@gmail.com>
2023-03-20 14:59:32 +0300

    add @Cacheable to findByName() method in LocationService

--- src/main/java/com/example/weatheranalyzer/service/LocationService.java
+++ src/main/java/com/example/weatheranalyzer/service/LocationService.java
@@ -2,7 +2,9 @@ package com.example.weatheranalyzer.service;
 
 import com.example.weatheranalyzer.entity.Location;
 import com.example.weatheranalyzer.repository.LocationRepository;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +14,7 @@ import java.util.Optional;
 public class LocationService {
     private final LocationRepository locationRepository;
 
+    @Cacheable(value = "locations")
     public Optional<Location> findByName(String name) {
         return locationRepository.findByCity(name);
     }
